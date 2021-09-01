c0aac8a0425b0ada6a1c3aa9ce1c59efa3630f7c
hguilfoyle <hguilfoyle1@gmail.com>
2021-09-01 11:33:30 -0400

    updated web.xml and added AppConfig (unfinished)

--- src/main/java/com/revature/spring_mvc/config/AppConfig.java
+++ src/main/java/com/revature/spring_mvc/config/AppConfig.java
@@ -1,4 +1,33 @@
 package com.revature.spring_mvc.config;
 
-public class AppConfig {
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+@EnableWebMvc
+@Configuration
+@ComponentScan("com.revature.spring_mvc")
+public class AppConfig implements WebMvcConfigurer, WebApplicationInitializer {
+
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        AnnotationConfigApplicationContext container = new AnnotationConfigApplicationContext();
+        container.register(AppConfig.class);
+
+//        servletContext.addListener(new ContextLoaderListener((WebApplicationContext) container));
+//        ServletRegistration.Dynamic dispatcher = servletContext.addServlet("DispatcherServlet", new DispatcherServlet(container));
+//        dispatcher.setLoadOnStartup(1);
+//        dispatcher.addMapping("/");
+    }
 }
