a2c55c277c7b3f1a41da1e3bfb341c5943e06348
raneyj <raney.jack99@gmail.com>
2021-09-02 10:51:40 -0500

    Added UserService

--- src/main/java/com/revature/jash/services/UserService.java
+++ src/main/java/com/revature/jash/services/UserService.java
@@ -1,4 +1,121 @@
 package com.revature.jash.services;
 
+import com.revature.jash.datasource.documents.User;
+import com.revature.jash.datasource.repositories.UserRepository;
+import com.revature.jash.util.PasswordUtils;
+import com.revature.jash.util.exceptions.AuthenticationException;
+import com.revature.jash.util.exceptions.InvalidRequestException;
+import com.revature.jash.util.exceptions.ResourceNotFoundException;
+import com.revature.jash.util.exceptions.ResourcePersistenceException;
+import com.revature.jash.web.dtos.UserDTO;
+import com.revature.jash.web.dtos.Principal;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
 public class UserService {
+
+    private final UserRepository userRepo;
+    private final PasswordUtils passwordUtils;
+
+    @Autowired
+    public UserService(UserRepository userRepo, PasswordUtils passwordUtils) {
+        this.userRepo = userRepo;
+        this.passwordUtils = passwordUtils;
+    }
+
+    public List<UserDTO> findAll() {
+        return userRepo.findAll()
+                .stream()
+                .map(UserDTO::new)
+                .collect(Collectors.toList());
+    }
+
+    public UserDTO findUserById(String id) {
+
+        if (id == null || id.trim().isEmpty()) {
+            throw new InvalidRequestException("Invalid id provided");
+        }
+
+        User user = userRepo.findById(id);
+
+        if (user == null) {
+            throw new ResourceNotFoundException();
+        }
+
+        return new UserDTO(user);
+
+    }
+
+    public User register(User newUser) {
+
+        if (!isUserValid(newUser)) {
+            throw new InvalidRequestException("Invalid user data provided!");
+        }
+
+        if (userRepo.findUserByUsername(newUser.getUsername()) != null) {
+            throw new ResourcePersistenceException("Provided username is already taken!");
+        }
+
+        if (userRepo.findUserByEmail(newUser.getEmail()) != null) {
+            throw new ResourcePersistenceException("Provided username is already taken!");
+        }
+
+        String encryptedPassword = passwordUtils.generateSecurePassword(newUser.getPassword());
+        newUser.setPassword(encryptedPassword);
+
+        return userRepo.save(newUser);
+
+    }
+
+    public Principal login(String username, String password) {
+
+        if (username == null || username.trim().equals("") || password == null || password.trim().equals("")) {
+            throw new InvalidRequestException("Invalid user credentials provided!");
+        }
+
+        String encryptedPassword = passwordUtils.generateSecurePassword(password);
+        User authUser = userRepo.findUserByCredentials(username, encryptedPassword);
+
+        if (authUser == null) {
+            throw new AuthenticationException("Invalid credentials provided!");
+        }
+
+        return new Principal(authUser);
+
+    }
+
+    public boolean isUsernameAvailable(String username) {
+
+        if (username == null || username.trim().equals("")) {
+            throw new InvalidRequestException("Invalid email value provided!");
+        }
+
+        return (userRepo.findUserByUsername(username) == null);
+    }
+
+    public boolean isEmailAvailable(String email) {
+
+        if (email == null || email.trim().equals("")) {
+            throw new InvalidRequestException("Invalid email value provided!");
+        }
+
+        return (userRepo.findUserByEmail(email) == null);
+    }
+
+    //TODO: Implement email verification
+    public boolean isUserValid(User user) {
+        if (user == null) return false;
+        if (user.getFirstName() == null || user.getFirstName().trim().equals("")) return false;
+        if (user.getLastName() == null || user.getLastName().trim().equals("")) return false;
+        if (user.getEmail() == null || user.getEmail().trim().equals("")) return false;
+        if (user.getUsername() == null || user.getUsername().trim().equals("")) return false;
+        return user.getPassword() != null && !user.getPassword().trim().equals("");
+    }
+
 }
+
--- src/main/java/com/revature/jash/web/dtos/UserDTO.java
+++ src/main/java/com/revature/jash/web/dtos/UserDTO.java
@@ -1,4 +1,88 @@
 package com.revature.jash.web.dtos;
 
+import com.revature.jash.datasource.documents.User;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
 public class UserDTO {
+
+    private String id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String username;
+
+    public UserDTO(User subject) {
+        this.id = subject.getId();
+        this.firstName = subject.getFirstName();
+        this.lastName = subject.getLastName();
+        this.email = subject.getEmail();
+        this.username = subject.getUsername();
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserDTO that = (UserDTO) o;
+        return Objects.equals(id, that.id) && Objects.equals(firstName, that.firstName) && Objects.equals(lastName, that.lastName) && Objects.equals(email, that.email) && Objects.equals(username, that.username);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, firstName, lastName, email, username);
+    }
+
+    @Override
+    public String toString() {
+        return "AppUserDTO{" +
+                "id='" + id + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", username='" + username + '\'' +
+                '}';
+    }
+
 }
