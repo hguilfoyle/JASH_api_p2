ef6e7648df6fc0b348d164ff2ae0c6751766fae9
AlfonsoHolmesData <alholmes@Email.peace.edu>
2021-09-01 21:05:08 -0400

    Question Pojo (Anotated)

--- src/main/java/com/revature/jash/datasource/documents/Question.java
+++ src/main/java/com/revature/jash/datasource/documents/Question.java
@@ -1,15 +1,14 @@
 package com.revature.jash.datasource.documents;
-
-
-
 import java.util.List;
 
 public class Question {
     private String Question;
     private String Answer;
-    private String Category;
-    private int difficultyLevel;
-    private float multiplier;
+    private String Category; // the type of question (comedy , movie , art etc..)
+    private int difficultyLevel;// (between 0 - 4) this property describes how hard the question is to the average person
+    private float pointValue; // (between 100 - 9999) this property denotes the amount of points this question is worth , if answered correctly
+    private float penaltyValue; // this property denotes the amount of points this question deducts , if answered incorrectly
+    private float multiplier; // (between 0.01 - 3.5) this property is a factor that we can use
     private List<String> hints; // TODO : may want to remove this , but for now i will leave it in
 
     public Question(String question, String answer, String category, int difficultyLevel) {
@@ -17,6 +16,20 @@ public class Question {
         Answer = answer;
         Category = category;
         this.difficultyLevel = difficultyLevel;
+    }
+    public Question(String question, String answer, String category, int difficultyLevel , float pointValue) {
+        new Question(question , answer , category , difficultyLevel);
+        this.pointValue = pointValue;
+    }
+
+    public Question(String question, String answer, String category, int difficultyLevel , float pointValue , float penaltyValue) {
+        new Question(question , answer , category , difficultyLevel , pointValue);
+        this.penaltyValue = penaltyValue;
+    }
+
+    public Question(String question, String answer, String category, int difficultyLevel, float pointValue, float penaltyValue, float multiplier) {
+        new Question(question , answer , category , difficultyLevel , pointValue , penaltyValue);
+        this.multiplier = multiplier;
     }
 
     public float getMultiplier() {
