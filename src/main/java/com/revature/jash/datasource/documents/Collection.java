673970abbb9fd4146037f7e8aa9c2091a2dac381
hguilfoyle <hguilfoyle1@gmail.com>
2021-09-03 17:18:44 -0400

    added collection pojo and question controller stuff

--- src/main/java/com/revature/jash/datasource/documents/Collection.java
+++ src/main/java/com/revature/jash/datasource/documents/Collection.java
@@ -4,6 +4,11 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Holds sets of questions
  * Names of Questions and Author
@@ -15,4 +20,29 @@ import org.springframework.data.mongodb.core.mapping.Document;
 public class Collection {
 
     private String id;
+
+    @NotBlank(message = "Title cannot be null or blank")
+    private String title;
+
+    @NotBlank(message = "Category cannot be null or blank")
+    private String category;
+
+    @NotBlank(message = "Author cannot be null or blank")
+    private User author;
+
+    @NotBlank(message = "Description cannot be null or blank")
+    private String description;
+
+    @NotEmpty(message = "Question List cannot be empty")
+    private List<Question> questionList = new ArrayList<>();
+
+    public Collection(String id, String title, String category, User author, String description, List<Question> questionList) {
+        this.id = id;
+        this.title = title;
+        this.category = category;
+        this.author = author;
+        this.description = description;
+        this.questionList = questionList;
+    }
+
 }
--- src/main/java/com/revature/jash/web/controllers/QuestionController.java
+++ src/main/java/com/revature/jash/web/controllers/QuestionController.java
@@ -1,10 +1,33 @@
 package com.revature.jash.web.controllers;
 
 
+import com.revature.jash.datasource.documents.Question;
+import com.revature.jash.datasource.documents.User;
+import com.revature.jash.services.QuestionService;
+import com.revature.jash.web.dtos.Principal;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
 /**
  * This Controller is specifically being used for the creation of custom questions by users.*/
 
-
+@RestController
+@RequestMapping("/questions")
+@CrossOrigin
 public class QuestionController {
+
+        private final QuestionService questionService;
+
+        public QuestionController(QuestionService questionService){
+            this.questionService = questionService;
+        }
+
+    @PostMapping(produces = "application/json")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Question createQuestion(@RequestBody @Valid Question newQuestion){
+            return questionService.createNewQuestion(newQuestion);
+    }
 
 }
