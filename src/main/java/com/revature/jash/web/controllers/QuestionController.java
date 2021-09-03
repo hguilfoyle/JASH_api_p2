0e8b3fba2e7eb2da84c3b416078277503acbb91d
hguilfoyle <hguilfoyle1@gmail.com>
2021-09-03 12:03:53 -0400

    added question controller

---
+++ src/main/java/com/revature/jash/web/controllers/QuestionController.java
@@ -0,0 +1,10 @@
+package com.revature.jash.web.controllers;
+
+
+/**
+ * This Controller is specifically being used for the creation of custom questions by users.*/
+
+
+public class QuestionController {
+
+}
